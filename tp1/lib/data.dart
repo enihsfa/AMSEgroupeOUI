f96f3ec9c051b8f19e45af1c5d9c1c7812c9f77c
enishfa <afshine.daurelle@netcourrier.com>
2024-02-12 17:51:36 +0100

    m

---
+++ tp1/lib/data.dart
@@ -0,0 +1,23 @@
+class MediaModel {
+  String imageUrl;
+  String title;;
+
+  // Constructor
+  MediaModel(
+      {this.imageUrl,
+      this.title,});
+}
+
+final stars = [
+  MediaModel(
+      imageUrl:
+          'https://hubblesite.org/files/live/sites/hubble/files/home/science/galaxies/_images/STScI-H-galaxies-0639a-2400x1200.jpg',
+      title: 'test'),
+]
+
+final galaxies = [
+  MediaModel(
+      imageUrl:
+          'https://hubblesite.org/files/live/sites/hubble/files/home/science/galaxies/_images/STScI-H-galaxies-0639a-2400x1200.jpg',
+      title: 'test'),
+]
---
+++ tp1/lib/main.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:tp1/data.dart';
+
+import 'data.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home',
+      theme: ThemeData(primaryColor: Colors.red[400]),
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _widgetOptions = <Widget>[
+    Text(
+      "Favoris ",
+      style: optionStyle,
+    ),
+    ListView.builder(
+      itemCount: stars.length,
+      itemBuilder: (context, index) {
+        return Card(
+          child: ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Description(stars, index),
+                ),
+              );
+            },
+            title: Text(stars[index].title),
+            leading: Image.network(stars[index].imageUrl),
+          ),
+        );
+      },
+    ),
+    ListView.builder(
+      itemCount: galaxies.length,
+      itemBuilder: (context, index) {
+        return Card(
+          child: ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Description(galaxies, index),
+                ),
+              );
+            },
+            title: Text(galaxies[index].title),
+            leading: Image.network(galaxies[index].imageUrl),
+          ),
+        );
+      },
+    ),
+    Text(
+      'TP1 \nDaurelle Afshine, Soete Jonathan',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gestion de média'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: 'stars',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: 'galaxies',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'Infos',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red[900],
+        unselectedItemColor: Colors.black87,
+        showUnselectedLabels: true,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+Widget Description(List<MediaModel> type, int index) {
+  return Scaffold(
+    appBar: AppBar(
+      automaticallyImplyLeading: true,
+      title: Text('${type[index].title}'),
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.favorite_border),
+            tooltip: 'Ajouter aux favoris',
+            onPressed: () {})
+      ],
+    ),
+    body: CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: <Widget>[
+        SliverAppBar(
+          leading: new Container(),
+          stretch: true,
+          onStretchTrigger: () {
+            return;
+          },
+          expandedHeight: 200.0,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Stack(
+              fit: StackFit.expand,
+              children: [
+                const DecoratedBox(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment(0.0, 0.5),
+                        end: Alignment(0.0, 0.0),
+                        colors: <Color>[
+                          Color(0xFFFFFFFF),
+                          Color(0xFFFFFFFF),
+                        ]),
+                  ),
+                ),
+                Image.network(
+                  type[index].imageUrl,
+                ),
+              ],
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(children(type, index)),
+        ),
+      ],
+    ),
+  );
+}
