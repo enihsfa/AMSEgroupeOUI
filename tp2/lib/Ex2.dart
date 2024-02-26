6b0a81983097666bac195b5b98afd8b2edad4a94
Jona0312 <jonathan.soete@etu.imt-nord-europe.fr>
2024-02-26 11:23:49 +0100

    Exo 2 et 4

---
+++ tp2/lib/Ex2.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class Exercice2 extends StatefulWidget {
+  @override
+  _Exercice2State createState() => _Exercice2State();
+}
+
+class _Exercice2State extends State<Exercice2> {
+  double rotationXValue = 0;
+  double rotationYValue = 0; // Nouveau
+  double rotationZValue = 0;
+  double scaleValue = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exercice 2'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Transform.rotate(
+              angle: rotationXValue * math.pi / 160,
+              child: Transform.rotate(
+                angle: rotationYValue * math.pi / 160, // Nouveau
+                child: Transform.rotate(
+                  angle: rotationZValue * math.pi / 160,
+                  child: Container(
+                    height: 450,
+                    width: 450,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(color: Colors.blue),
+                    child: Image(image: NetworkImage('https://picsum.photos/512')),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Slider(
+            min: 0.0,
+            max: 100.0,
+            activeColor: Colors.blue,
+            inactiveColor: Colors.grey,
+            value: rotationXValue,
+            label: rotationXValue.round().toString(),
+            onChanged: (double newXvalue) {
+              setState(() {
+                rotationXValue = newXvalue;
+              });
+            },
+          ),
+          Slider(
+            min: 0.0,
+            max: 100.0,
+            activeColor: Colors.blue,
+            inactiveColor: Colors.grey,
+            value: rotationYValue,
+            label: rotationYValue.round().toString(),
+            onChanged: (double newYvalue) { // Nouveau
+              setState(() {
+                rotationYValue = newYvalue;
+              });
+            },
+          ),
+          Slider(
+            min: 0.0,
+            max: 100.0,
+            activeColor: Colors.blue,
+            inactiveColor: Colors.grey,
+            value: rotationZValue,
+            label: rotationZValue.round().toString(),
+            onChanged: (double newZvalue) {
+              setState(() {
+                rotationZValue = newZvalue;
+              });
+            },
+          ),
+          Slider(
+            min: 0.0,
+            max: 100.0,
+            activeColor: Colors.blue,
+            inactiveColor: Colors.grey,
+            value: scaleValue,
+            label: scaleValue.round().toString(),
+            onChanged: (double newScalevalue) {
+              setState(() {
+                scaleValue = newScalevalue;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ tp2/lib/Ex4.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class Tile {
+  String imageURL;
+
+  Tile({required this.imageURL});
+
+  Widget croppedImageTile() {
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Container(
+          child: Align(
+            alignment: Alignment.centerLeft,
+            widthFactor: 0.3,
+            heightFactor: 0.3,
+            child: Image.network(this.imageURL),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget croppedImageTile2(int index, int taille) {
+    int q = index ~/ taille;
+    int r = index % taille;
+    int n = taille - 1;
+
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Container(
+          child: Align(
+            alignment: FractionalOffset(r / n, q / n),
+            widthFactor: 1 / taille,
+            heightFactor: 1 / taille,
+            child: Image.network(this.imageURL),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Tile tile2 = new Tile(imageURL: 'https://picsum.photos/512');
+
+class DisplayTileWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Affichage d'une tuile"),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(children: [
+        SizedBox(
+            width: 150.0,
+            height: 150.0,
+            child: Container(
+                margin: EdgeInsets.all(20.0),
+                child: this.createTileWidgetFrom(tile2))),
+        Container(
+            height: 200,
+            child:
+                Image.network('https://picsum.photos/512', fit: BoxFit.cover))
+      ])),
+    );
+  }
+
+  Widget createTileWidgetFrom(Tile tile) {
+    return InkWell(
+      child: tile.croppedImageTile(),
+      onTap: () {
+        print("tapped on tile");
+      },
+    );
+  }
+}
