e7ca648e90b2dc6fd8c278032bcefb4d29b7530a
Jona0312 <jonathan.soete@etu.imt-nord-europe.fr>
2024-02-26 15:03:51 +0100

    curseurs fixes

--- tp2/lib/Ex2.dart
+++ tp2/lib/Ex2.dart
@@ -20,32 +20,7 @@ class _Exercice2State extends State<Exercice2> {
       ),
       body: Column(
         children: <Widget>[
-          
-          SizedBox(height: 20),
-          Center(
-            child: Transform(
-              transform: Matrix4.identity()
-                ..setEntry(3, 2, 0.001) // perspective
-                ..rotateX(rotationXValue * math.pi / 180)
-                ..rotateY(rotationYValue * math.pi / 180)
-                ..rotateZ(rotationZValue * math.pi / 180),
-              alignment: FractionalOffset.center,
-              child: Container(
-                constraints: BoxConstraints(
-                  minWidth: 1,   // Largeur minimale du container
-                  maxWidth: 420,   // Largeur maximale du container
-                  minHeight: 1,   // Hauteur minimale du container
-                  maxHeight: 420,  // Hauteur maximale du container
-                ),
-                height: 250 * scaleValue,
-                width: 250 * scaleValue,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(color: Colors.blue),
-                child: Image(image: NetworkImage('https://cdn.ebaumsworld.com/mediaFiles/picture/718392/84890872.png')),
-              ),
-            ),
-          ),
-
+         
           SizedBox(height: 20),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -146,7 +121,32 @@ class _Exercice2State extends State<Exercice2> {
               ],
             ),
           ),
-          
+           
+          SizedBox(height: 20),
+          Center(
+            child: Transform(
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.001) // perspective
+                ..rotateX(rotationXValue * math.pi / 180)
+                ..rotateY(rotationYValue * math.pi / 180)
+                ..rotateZ(rotationZValue * math.pi / 180),
+              alignment: FractionalOffset.center,
+              child: Container(
+                constraints: BoxConstraints(
+                  minWidth: 1,   // Largeur minimale du container
+                  maxWidth: 420,   // Largeur maximale du container
+                  minHeight: 1,   // Hauteur minimale du container
+                  maxHeight: 420,  // Hauteur maximale du container
+                ),
+                height: 250 * scaleValue,
+                width: 250 * scaleValue,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Image(image: NetworkImage('https://cdn.ebaumsworld.com/mediaFiles/picture/718392/84890872.png')),
+              ),
+            ),
+          ),
+
         ],
       ),
     );
