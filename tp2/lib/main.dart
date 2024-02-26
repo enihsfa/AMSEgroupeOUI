f5252375103f0d69da626a85543b5e018088dfa8
enishfa <afshine.daurelle@netcourrier.com>
2024-02-26 09:29:13 +0100

    m

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'Ex1.dart';
+/*import 'Ex2.dart';
+import 'Ex4.dart';
+import 'Ex5.dart';
+import 'Ex6.dart';
+import 'Ex7.dart';*/
+
 
 void main() {
   runApp(MyApp());
@@ -50,6 +56,120 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                 subtitle: Text('Afficher une image'),
               ),
             ),
+            /*
+            Card( 
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Exercice2(),
+                    ),
+                  );
+                },
+                title: Text('Exercice 2'),
+                subtitle: Text('Transformer une image'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DisplayTileWidget(),
+                    ),
+                  );
+                },
+                title: Text('Exercice 4'),
+                subtitle: Text("Affichage d'une tuile"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Exercice5a(),
+                    ),
+                  );
+                },
+                title: Text('Exercice 5a'),
+                subtitle: Text("Génération du plateau de tuiles"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Exercice5b(),
+                    ),
+                  );
+                },
+                title: Text('Exercice 5b'),
+                subtitle: Text(
+                    "Génération du plateau de tuiles à partir d'une image"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Exercice5c(),
+                    ),
+                  );
+                },
+                title: Text('Exercice 5c'),
+                subtitle: Text("Configuration du plateau de tuiles"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PositionedTiles(),
+                    ),
+                  );
+                },
+                title: Text('Exercice 6a'),
+                subtitle: Text("Echange de deux tuiles"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Exercice6b(),
+                    ),
+                  );
+                },
+                title: Text('Exercice 6b'),
+                subtitle: Text("Echange de deux tuiles dans un plateau"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => GameTaquin(),
+                    ),
+                  );
+                },
+                title: Text('Exercice 7'),
+                subtitle: Text("Jeu du Taquin"),
+              ),
+            ),*/
           ],
         ));
   }
